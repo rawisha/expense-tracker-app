780fe62f607e4bb8a5f330729887d62a0e7f8686
rawisha <rawvsmaw22@gmail.com>
2025-03-05 19:38:16 +0100

    welcome screen

---
+++ components/Loading.tsx
@@ -0,0 +1,14 @@
+import { colors } from '@/constants/theme'
+import { ActivityIndicator, ActivityIndicatorProps, StyleSheet, Text, View } from 'react-native'
+const Loading = ({
+    size = "large",
+    color = colors.primary
+}: ActivityIndicatorProps) => {
+  return (
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <ActivityIndicator size={size} color={color} />
+    </View>
+  )
+}
+export default Loading
+const styles = StyleSheet.create({})
